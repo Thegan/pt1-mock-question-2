291e88138a6185c25603928a323180797230b247
Thegan <thegan.moodley1@gmail.com>
2015-04-14 16:14:07 +0200

    Q2

---
+++ time.h
@@ -0,0 +1,24 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include<iostream>
+
+class Time
+{
+	private:
+
+	int hours;
+	int minutes;
+
+public:
+	Time(int hours , int minutes );
+	~Time();
+
+	void set(int hours, int minutes);
+	void get(int &hours, int &minutes);
+
+	Time operator++(int);
+	friend std::ostream & operator <<(std::ostringstream &os, Time &t);
+};
+#endif
+
